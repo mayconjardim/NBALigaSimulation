2634d72a4d2dfaff0566bfe1d4b8b3a0c2cc1a85
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-05-04 16:21:55 -0300

    Ajustando PlayerHeader css

---
+++ NBALigaSimulation/Client/Pages/Player/PlayerRatings.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using NBALigaSimulation.Shared.Models;
+
+namespace NBALigaSimulation.Client.Pages.Player
+{
+    partial class PlayerRatings
+    {
+        [Parameter]
+        public PlayerCompleteDto? player { get; set; }
+
+        private PlayerRatingDto? rating => player.Ratings.LastOrDefault();
+
+    }
+}
