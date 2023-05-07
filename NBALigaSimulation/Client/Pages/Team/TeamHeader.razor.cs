6f189ce9599515466ade97440b7855764f194120
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-05-07 14:39:52 -0300

    Estilizando TeamHeader

---
+++ NBALigaSimulation/Client/Pages/Team/TeamHeader.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NBALigaSimulation.Client.Pages.Team
+{
+    partial class TeamHeader
+    {
+
+        [Parameter]
+        public TeamCompleteDto? team { get; set; }
+
+    }
+}
