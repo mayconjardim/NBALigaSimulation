48cc777d6b64966d6177ff93e81531854d2de26f
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-05-08 21:59:55 -0300

    Criando e estilizando card

--- NBALigaSimulation/Client/Pages/Team/TeamRoster.razor.cs
+++ NBALigaSimulation/Client/Pages/Team/TeamRoster.razor.cs
@@ -1,6 +1,12 @@
-﻿namespace NBALigaSimulation.Client.Pages.Team
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NBALigaSimulation.Client.Pages.Team
 {
     partial class TeamRoster
     {
+
+        [Parameter]
+        public TeamCompleteDto? team { get; set; }
+
     }
 }
