ab931883617784f9aec23fffa50699ed355534a5
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-05-10 16:35:34 -0300

    Criando entidade Game

---
+++ NBALigaSimulation/Shared/Models/Game/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NBALigaSimulation.Shared.Models
+{
+    public class Game
+    {
+
+        public int Id { get; set; }
+        public int HomeTeamId { get; set; }
+        [ForeignKey("HomeTeamId")]
+        public Team HomeTeam { get; set; }
+
+        public int AwayTeamId { get; set; }
+        [ForeignKey("AwayTeamId")]
+        public Team AwayTeam { get; set; }
+        public int HomeQ1 { get; set; }
+        public int HomeQ2 { get; set; }
+        public int HomeQ3 { get; set; }
+        public int HomeQ4 { get; set; }
+        public int HomeOT { get; set; }
+        public int AwayQ1 { get; set; }
+        public int AwayQ2 { get; set; }
+        public int AwayQ3 { get; set; }
+        public int AwayQ4 { get; set; }
+        public int AwayOT { get; set; }
+        public List<string> PlayByPlay { get; set; }
+
+    }
+}
