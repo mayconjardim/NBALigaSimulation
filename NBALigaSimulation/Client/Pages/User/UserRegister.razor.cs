022e180936fff744ddcf96b0499aa6578798d7b9
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-03 23:46:16 -0300

    Adicionando Model UserRegistration e Page UserRegister no Client

---
+++ NBALigaSimulation/Client/Pages/User/UserRegister.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace NBALigaSimulation.Client.Pages.User
+{
+    partial class UserRegister
+    {
+
+        UserRegistration user = new UserRegistration();
+
+        void HandleRegistration()
+        {
+            Console.WriteLine($"Registrando um user: {user.Username}");
+        }
+
+    }
+}
---
+++ NBALigaSimulation/Shared/Models/User/UserRegistration.cs
@@ -0,0 +1,11 @@
+﻿namespace NBALigaSimulation.Shared.Models
+{
+    public class UserRegistration
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
