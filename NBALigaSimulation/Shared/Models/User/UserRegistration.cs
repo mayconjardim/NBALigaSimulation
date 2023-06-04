6c729b58d84c30403ad546cd6331b5b9174bd245
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-04 20:59:38 -0300

    Adicionando validação em UserRegistration

--- NBALigaSimulation/Shared/Models/User/UserRegistration.cs
+++ NBALigaSimulation/Shared/Models/User/UserRegistration.cs
@@ -1,10 +1,17 @@
-﻿namespace NBALigaSimulation.Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NBALigaSimulation.Shared.Models
 {
     public class UserRegistration
     {
 
+        [Required]
         public string Username { get; set; }
+        [Required]
+
         public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "A senhas não são iguais!")]
         public string ConfirmPassword { get; set; }
 
     }
