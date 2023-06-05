915536092ed93a167791a8033538c440d37c01f1
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-05 11:58:06 -0300

    Criando AuthService Contrato e Implementação no Cliente

---
+++ NBALigaSimulation/Client/Services/AuthService/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using NBALigaSimulation.Client.Pages.User;
+
+namespace NBALigaSimulation.Client.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly HttpClient _http;
+
+        public AuthService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<ServiceResponse<int>> Register(UserRegister request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/register", request);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+        }
+
+    }
+}
---
+++ NBALigaSimulation/Client/Services/AuthService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using NBALigaSimulation.Client.Pages.User;
+
+namespace NBALigaSimulation.Client.Services.AuthService
+{
+    public interface IAuthService
+    {
+
+        Task<ServiceResponse<int>> Register(UserRegister request);
+
+    }
+}
