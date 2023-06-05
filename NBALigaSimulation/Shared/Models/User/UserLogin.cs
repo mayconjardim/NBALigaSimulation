ec572df8888aec06eac2caa3f8181a6f3d1f645c
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-05 12:49:34 -0300

    Criando classe de Login

---
+++ NBALigaSimulation/Shared/Models/User/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NBALigaSimulation.Shared.Models
+{
+    public class UserLogin
+    {
+
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
