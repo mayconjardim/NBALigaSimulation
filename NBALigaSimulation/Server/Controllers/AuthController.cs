c9b6d1ac1ef7b658f872e751360454ae36b0dec6
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-06 22:42:36 -0300

    Criando metodo para atualizar senha no Controller

--- NBALigaSimulation/Server/Controllers/AuthController.cs
+++ NBALigaSimulation/Server/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace NBALigaSimulation.Server.Controllers
 {
@@ -44,5 +46,20 @@ namespace NBALigaSimulation.Server.Controllers
             return Ok(response);
 
         }
+
+        [HttpPost("change-password"), Authorize]
+        public async Task<ActionResult<ServiceResponse<bool>>> ChangePassword([FromBody] string newPassword)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var response = await _authService.ChangePassword(int.Parse(userId), newPassword);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
     }
 }
