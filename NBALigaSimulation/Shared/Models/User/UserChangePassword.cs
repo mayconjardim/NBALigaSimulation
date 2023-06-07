f0b28be2b1c2d5c5baaec7d194b0b1fa8952473d
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-06 22:36:17 -0300

    Criando classe para mudar a senha do usuario

---
+++ NBALigaSimulation/Shared/Models/User/UserChangePassword.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NBALigaSimulation.Shared.Models
+{
+    public class UserChangePassword
+    {
+
+        [Required, StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+        [Compare("Password", ErrorMessage = "A senhas não são iguais!")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
