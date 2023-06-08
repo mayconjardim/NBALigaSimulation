597328cb184ae4b604756e4c3d7115acf98a04e4
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-08 20:15:53 -0300

    Criando pagina para trocar senha no Cliente

---
+++ NBALigaSimulation/Client/Pages/User/UserChangePasswords.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace NBALigaSimulation.Client.Pages.User
+{
+    partial class UserChangePasswords
+    {
+
+        UserChangePassword request = new UserChangePassword();
+        string message = string.Empty;
+
+        private async Task ChangePassword()
+        {
+            var result = await AuthService.ChangePassword(request);
+            message = result.Message;
+        }
+
+    }
+}
