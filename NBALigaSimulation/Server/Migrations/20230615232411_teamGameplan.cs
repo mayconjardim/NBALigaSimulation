faa406f6802532e1a58640c92de6ddd2213d471c
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-15 20:29:16 -0300

    Criando migração de TeamGameplan

---
+++ NBALigaSimulation/Server/Migrations/20230615232411_teamGameplan.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBALigaSimulation.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class teamGameplan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.CreateTable(
+                name: "TeamGameplans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TeamId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Pace = table.Column<double>(type: "REAL", nullable: false),
+                    Focus = table.Column<int>(type: "INTEGER", nullable: false),
+                    Motion = table.Column<int>(type: "INTEGER", nullable: false),
+                    Defense = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamGameplans", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeamGameplans_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamGameplans_TeamId",
+                table: "TeamGameplans",
+                column: "TeamId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeamGameplans");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
