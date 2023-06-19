f046a234bd427e8c41d9fc439ca2479cb8778bf0
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-19 14:32:16 -0300

    Criando TradeService no Client

---
+++ NBALigaSimulation/Client/Services/TradeService/ITradeService.cs
@@ -0,0 +1,10 @@
+﻿namespace NBALigaSimulation.Client.Services.TradeService
+{
+    public interface ITradeService
+    {
+
+        Task<ServiceResponse<TradeDto>> GetTradeByTeamId(int teamId);
+        Task<ServiceResponse<List<TradeDto>>> GetAllTrades();
+
+    }
+}
---
+++ NBALigaSimulation/Client/Services/TradeService/TradeService.cs
@@ -0,0 +1,27 @@
+﻿namespace NBALigaSimulation.Client.Services.TradeService
+{
+    public class TradeService : ITradeService
+    {
+
+        private readonly HttpClient _http;
+
+        public TradeService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public string Message { get; set; } = "Carregando Trades...";
+
+        public async Task<ServiceResponse<List<TradeDto>>> GetAllTrades()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<TradeDto>>>($"api/trades");
+            return result;
+        }
+
+        public async Task<ServiceResponse<TradeDto>> GetTradeByTeamId(int teamId)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<TradeDto>>($"api/trades/{teamId}");
+            return result;
+        }
+    }
+}
