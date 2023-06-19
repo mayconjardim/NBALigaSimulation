f0015755f2538cf44e6b88e966525bf8ec23f4ad
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-19 12:16:21 -0300

    Criando Classe Trade

---
+++ NBALigaSimulation/Shared/Models/Trade/Trade.cs
@@ -0,0 +1,19 @@
+﻿namespace NBALigaSimulation.Shared.Models.Trade
+{
+    public class Trade
+    {
+
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public int TeamOneId { get; set; }
+        public Team TeamOne { get; set; }
+
+        public int TeamTwoId { get; set; }
+        public Team TeamTwo { get; set; }
+
+        public List<Player> PlayersTeamOne { get; set; }
+        public List<Player> PlayersTeamTwo { get; set; }
+
+    }
+}
