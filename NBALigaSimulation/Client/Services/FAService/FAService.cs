eab5956356cd219929bed4194a9c06afb8be4a9d
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-30 11:45:32 -0300

    Criando FAService contrato e imp no Client

---
+++ NBALigaSimulation/Client/Services/FAService/FAService.cs
@@ -0,0 +1,47 @@
+﻿using NBALigaSimulation.Shared.Dtos;
+
+namespace NBALigaSimulation.Client.Services.FAService
+{
+    public class FAService : IFAService
+    {
+
+        private readonly HttpClient _http;
+
+        public FAService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<ServiceResponse<FAOfferDto>> CreateOffer(FAOfferDto offerDto)
+        {
+            var response = new ServiceResponse<FAOfferDto>();
+
+            var result = await _http.PostAsJsonAsync("api/fa", offerDto);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var tradeResponse = await result.Content.ReadFromJsonAsync<ServiceResponse<FAOfferDto>>();
+
+                if (tradeResponse.Success)
+                {
+                    response.Success = true;
+                    response.Data = tradeResponse.Data;
+                    response.Message = "Trade created successfully.";
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = tradeResponse.Message;
+                }
+            }
+            else
+            {
+                response.Success = false;
+                response.Message = "Failed to create trade. HTTP status code: " + result.StatusCode;
+            }
+
+            return response;
+        }
+
+    }
+}
---
+++ NBALigaSimulation/Client/Services/FAService/IFAService.cs
@@ -0,0 +1,9 @@
+﻿namespace NBALigaSimulation.Client.Services.FAService
+{
+    public interface IFAService
+    {
+
+        Task<ServiceResponse<FAOfferDto>> CreateOffer(FAOfferDto offerDto);
+
+    }
+}
