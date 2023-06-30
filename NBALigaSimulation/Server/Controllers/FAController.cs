62b1003da31e2cc8dacf3309db0622a4d4384209
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-06-30 11:26:31 -0300

    Criando FAController

---
+++ NBALigaSimulation/Server/Controllers/FAController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NBALigaSimulation.Server.Controllers
+{
+    [Route("api/fa")]
+    [ApiController]
+    public class FAController : ControllerBase
+    {
+
+        private readonly IFAService _faService;
+
+        public FAController(IFAService faService)
+        {
+            _faService = faService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateOffer(FAOfferDto offerDto)
+        {
+            var response = await _faService.CreateOffer(offerDto);
+
+            if (!response.Success)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+    }
+
+}
