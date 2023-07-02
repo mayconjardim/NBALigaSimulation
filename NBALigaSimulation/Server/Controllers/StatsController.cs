5c331744fe5fd60eada6e1859f9f9aadc8c2d59a
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-02 20:08:56 -0300

    Criando StatsController

---
+++ NBALigaSimulation/Server/Controllers/StatsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NBALigaSimulation.Server.Services.StatsService;
+
+namespace NBALigaSimulation.Server.Controllers
+{
+    [Route("api/stats")]
+    [ApiController]
+    public class StatsController : ControllerBase
+    {
+
+        private readonly IStatsService _statsService;
+
+        public StatsController(IStatsService statsService)
+        {
+            _statsService = statsService;
+        }
+
+        [HttpGet("teams")]
+        public async Task<ActionResult<ServiceResponse<List<TeamRegularStatsDto>>>> GetAllTeamRegularStats()
+        {
+            var result = await _statsService.GetAllTeamRegularStats();
+            return Ok(result);
+        }
+
+    }
+}
