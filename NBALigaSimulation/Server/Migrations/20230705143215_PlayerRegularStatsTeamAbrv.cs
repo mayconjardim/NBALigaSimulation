7eb9b86323fe725ec283c4c699253beccb0c8717
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-05 11:33:06 -0300

    Adicionando TeamAbrv a  PlayerRegularStats e realizando a migração

---
+++ NBALigaSimulation/Server/Migrations/20230705143215_PlayerRegularStatsTeamAbrv.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBALigaSimulation.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayerRegularStatsTeamAbrv : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TeamAbrv",
+                table: "PlayerRegularStats",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamAbrv",
+                table: "PlayerRegularStats");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
