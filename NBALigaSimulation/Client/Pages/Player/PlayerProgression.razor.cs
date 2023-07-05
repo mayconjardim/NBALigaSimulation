d0778075d4d16c6b21db1d6c7bab2a97bdc766d4
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-05 15:27:50 -0300

    Criando Page de PlayerProgression no Client

---
+++ NBALigaSimulation/Client/Pages/Player/PlayerProgression.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NBALigaSimulation.Client.Pages.Player
+{
+    partial class PlayerProgression
+    {
+
+        [Parameter]
+        public PlayerCompleteDto? player { get; set; }
+
+        private List<PlayerRatingDto>? ratings { get; set; }
+
+        protected override void OnInitialized()
+        {
+            ratings = player.Ratings;
+        }
+
+    }
+}
