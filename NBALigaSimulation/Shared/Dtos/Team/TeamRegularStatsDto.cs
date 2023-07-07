437e5585fe4280c3b9280b6dcd4099a4af7f2af4
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-07 15:10:39 -0300

    Criando Page TeamLeaders no Client

--- NBALigaSimulation/Shared/Dtos/Team/TeamRegularStatsDto.cs
+++ NBALigaSimulation/Shared/Dtos/Team/TeamRegularStatsDto.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
 using System.Net;
+using System.Runtime.Intrinsics.X86;
+using static System.Net.WebRequestMethods;
 
 namespace NBALigaSimulation.Shared.Dtos
 {
@@ -110,6 +112,197 @@ namespace NBALigaSimulation.Shared.Dtos
                 int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
                 double diff = (double)(Points - AllowedPoints) / games;
                 return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string PtsPG
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)Points / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string AllowedPtsPG
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)AllowedPoints / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string RebPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)Rebounds / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string AstPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)Assists / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string BlkPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)Blocks / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string StlPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)Steals / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string TOPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)Turnovers / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string FgPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)FGM / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+        public string FgaPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)FGA / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string FgPct
+        {
+            get
+            {
+                if (FGA == 0)
+                {
+                    return "0.0";
+                }
+
+                double percentage = (double)FGM / FGA * 100;
+                return percentage.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string TPPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)TPM / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+        public string TPaPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)TPA / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string TPPct
+        {
+            get
+            {
+                if (TPA == 0)
+                {
+                    return "0.0";
+                }
+
+                double percentage = (double)TPM / TPA * 100;
+                return percentage.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string FTPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)FTM / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+        public string FTaPg
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double diff = (double)FTA / games;
+                return diff.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string FTPct
+        {
+            get
+            {
+                if (FTA == 0)
+                {
+                    return "0.0";
+                }
+
+                double percentage = (double)FTM / FTA * 100;
+                return percentage.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string OffensiveEfficiency
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double per = (1.0 / games) * ((Points * 85.910) + (Rebounds * 53.840) + (Assists * 34.677) + (Steals * 53.840) + (Blocks * 53.840) - (FGA * 39.190) - (FTA * 20.091) - (Turnovers * 53.840));
+                double stat = per / 100;
+                return stat.ToString("0.0", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public string DefensiveEfficiency
+        {
+            get
+            {
+                int games = HomeWins + HomeLosses + RoadWins + RoadLosses;
+                double per = (1.0 / games) * ((AllowedPoints * 85.910) + (AllowedRebounds * 53.840) + (AllowedAssists * 34.677) + (AllowedStealS * 53.840) + (AllowedBlocks * 53.840) - (AllowedFGA * 39.190) - (AllowedFTA * 20.091) - (AllowedTurnovers * 53.840));
+                double stat = per / 100;
+                return stat.ToString("0.0", CultureInfo.InvariantCulture);
             }
         }
 
