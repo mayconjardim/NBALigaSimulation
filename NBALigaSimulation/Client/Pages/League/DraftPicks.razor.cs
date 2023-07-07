9cdf0881b5c44ddd5ff85d73d6620ced51bb422d
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-07 16:52:17 -0300

    Criando Page Finances no Client

---
+++ NBALigaSimulation/Client/Pages/League/DraftPicks.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace NBALigaSimulation.Client.Pages.League
+{
+    partial class DraftPicks
+    {
+        public List<TeamDraftPickDto>? picks { get; set; }
+
+        string[] headings = { "YEAR", "ROUND", "TEAM", "ORIGINAL" };
+
+        public string message { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            message = "Carregando Draft Picks...";
+
+
+            var result = await LeagueService.GetAllDraftPicks();
+            if (!result.Success)
+            {
+                message = result.Message;
+            }
+            else
+            {
+                picks = result.Data;
+            }
+        }
+
+
+    }
+}
