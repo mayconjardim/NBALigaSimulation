c5393575b18c34a1b3f8822922b1af70b97c29c5
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-09 13:09:46 -0300

    Criando Classe Playoffs

---
+++ NBALigaSimulation/Shared/Models/Playoffs/Playoffs.cs
@@ -0,0 +1,20 @@
+﻿namespace NBALigaSimulation.Shared.Models.Playoffs
+{
+    public class Playoffs
+    {
+
+        public int Id { get; set; }
+        public int Season { get; set; }
+        public int SeriesId { get; set; }
+        public bool? Complete { get; set; }
+        public int TeamOneId { get; set; }
+        public Team TeamOne { get; set; }
+
+        public int TeamTwoId { get; set; }
+        public Team TeamTwo { get; set; }
+
+        public int WinsTeamOne { get; set; }
+        public int WinsTeamTwo { get; set; }
+
+    }
+}
