4cb1cb48a64f8c78ace1232b9f67d2ecc5ff02a9
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-13 14:20:54 -0300

    Criando classe PlayoffsGame pra identificar jogos de uma serie

---
+++ NBALigaSimulation/Server/Migrations/20230713171901_PlayoffsGame.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBALigaSimulation.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayoffsGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.CreateTable(
+                name: "PlayoffGames",
+                columns: table => new
+                {
+                    PlayoffsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    GameId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayoffGames", x => new { x.PlayoffsId, x.GameId });
+                    table.ForeignKey(
+                        name: "FK_PlayoffGames_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlayoffGames_Playoffs_PlayoffsId",
+                        column: x => x.PlayoffsId,
+                        principalTable: "Playoffs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayoffGames_GameId",
+                table: "PlayoffGames",
+                column: "GameId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayoffGames");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
--- NBALigaSimulation/Shared/Models/Playoffs/Playoffs.cs
+++ NBALigaSimulation/Shared/Models/Playoffs/Playoffs.cs
@@ -1,20 +1,22 @@
 ﻿namespace NBALigaSimulation.Shared.Models
 {
-    public class Playoffs
-    {
+	public class Playoffs
+	{
 
-        public int Id { get; set; }
-        public int Season { get; set; }
-        public int SeriesId { get; set; }
-        public bool? Complete { get; set; }
-        public int TeamOneId { get; set; }
-        public Team TeamOne { get; set; }
+		public int Id { get; set; }
+		public int Season { get; set; }
+		public int SeriesId { get; set; }
+		public bool? Complete { get; set; }
+		public int TeamOneId { get; set; }
+		public Team TeamOne { get; set; }
 
-        public int TeamTwoId { get; set; }
-        public Team TeamTwo { get; set; }
+		public int TeamTwoId { get; set; }
+		public Team TeamTwo { get; set; }
 
-        public int WinsTeamOne { get; set; }
-        public int WinsTeamTwo { get; set; }
+		public int WinsTeamOne { get; set; }
+		public int WinsTeamTwo { get; set; }
 
-    }
+		public List<PlayoffsGame> PlayoffGames { get; set; } = new List<PlayoffsGame>();
+
+	}
 }
