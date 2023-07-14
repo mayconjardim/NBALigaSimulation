f501098dd6efbc9d8b2d8d37637587976b4a702b
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-14 15:37:02 -0300

    Criando DraftService contrato  e  Implementação  no Client

---
+++ NBALigaSimulation/Client/Services/DraftService/DraftService.cs
@@ -0,0 +1,24 @@
+﻿namespace NBALigaSimulation.Client.Services.DraftService
+{
+	public class DraftService : IDraftService
+	{
+		private readonly HttpClient _http;
+
+		public DraftService(HttpClient http)
+		{
+			_http = http;
+		}
+
+		public Task<ServiceResponse<bool>> GenerateLottery()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<ServiceResponse<DraftLotteryDto>> GetLastLottery()
+		{
+			var response = await _http.GetFromJsonAsync<ServiceResponse<DraftLotteryDto>>($"api/draft/lottery");
+			return response;
+		}
+
+	}
+}
---
+++ NBALigaSimulation/Client/Services/DraftService/IDraftService.cs
@@ -0,0 +1,10 @@
+﻿namespace NBALigaSimulation.Client.Services.DraftService
+{
+	public interface IDraftService
+	{
+
+		Task<ServiceResponse<DraftLotteryDto>> GetLastLottery();
+		Task<ServiceResponse<bool>> GenerateLottery();
+
+	}
+}
