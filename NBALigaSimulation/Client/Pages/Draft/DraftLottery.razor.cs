8323a4f9d336a768a7122a4cc67b0d81c7bfecc7
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-14 18:36:21 -0300

    Criando  Page  Draft Lottery no Client

---
+++ NBALigaSimulation/Client/Pages/Draft/DraftLottery.razor.cs
@@ -0,0 +1,26 @@
+﻿namespace NBALigaSimulation.Client.Pages.Draft
+{
+    partial class DraftLottery
+    {
+        private DraftLotteryDto? lottery = null;
+
+        private string message;
+        protected override async Task OnInitializedAsync()
+        {
+            message = "Carregando LOTERIA...";
+
+            var result = await DraftService.GetLastLottery();
+            if (!result.Success)
+            {
+                message = result.Message;
+            }
+            else
+            {
+                lottery = result.Data;
+
+            }
+        }
+
+
+    }
+}
--- NBALigaSimulation/Server/Controllers/DraftController.cs
+++ NBALigaSimulation/Server/Controllers/DraftController.cs
@@ -3,41 +3,41 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace NBALigaSimulation.Server.Controllers
 {
-	[Route("api/draft")]
-	[ApiController]
-	public class DraftController : ControllerBase
-	{
+    [Route("api/draft")]
+    [ApiController]
+    public class DraftController : ControllerBase
+    {
 
-		private readonly IDraftService _draftService;
+        private readonly IDraftService _draftService;
 
-		public DraftController(IDraftService draftService)
-		{
-			_draftService = draftService;
-		}
+        public DraftController(IDraftService draftService)
+        {
+            _draftService = draftService;
+        }
 
-		[HttpGet]
-		[SwaggerOperation(Summary = "GetLastLottery", Description = "Retorna a ultima loteria realizada.")]
-		public async Task<ActionResult<ServiceResponse<DraftLotteryDto>>> GetLastLottery()
-		{
+        [HttpGet("lotto")]
+        [SwaggerOperation(Summary = "GetLastLottery", Description = "Retorna a ultima loteria realizada.")]
+        public async Task<ActionResult<ServiceResponse<DraftLotteryDto>>> GetLastLottery()
+        {
 
-			var result = await _draftService.GetLastLottery();
+            var result = await _draftService.GetLastLottery();
 
-			if (!result.Success)
-			{
-				return NotFound(result);
-			}
+            if (!result.Success)
+            {
+                return NotFound(result);
+            }
 
-			return Ok(result);
+            return Ok(result);
 
-		}
+        }
 
-		[HttpPost("lottery")]
-		public async Task<ActionResult<ServiceResponse<bool>>> GenerateLottery()
-		{
-			var result = await _draftService.GenerateLottery();
-			return Ok(result);
-		}
+        [HttpPost("generate/lottery")]
+        public async Task<ActionResult<ServiceResponse<bool>>> GenerateLottery()
+        {
+            var result = await _draftService.GenerateLottery();
+            return Ok(result);
+        }
 
 
-	}
+    }
 }
