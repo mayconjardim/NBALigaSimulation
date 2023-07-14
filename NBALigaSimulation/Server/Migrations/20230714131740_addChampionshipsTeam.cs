75bc814a426c7dd740868bc66bd3700f1793258f
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-14 10:19:25 -0300

    Adicionando Championships na  classe Team e criando migração

---
+++ NBALigaSimulation/Server/Migrations/20230714131740_addChampionshipsTeam.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBALigaSimulation.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addChampionshipsTeam : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Championships",
+                table: "Teams",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Championships",
+                table: "Teams");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
--- NBALigaSimulation/Shared/Models/Team/Team.cs
+++ NBALigaSimulation/Shared/Models/Team/Team.cs
@@ -3,27 +3,28 @@
 namespace NBALigaSimulation.Shared.Models
 {
 
-    public class Team
-    {
+	public class Team
+	{
 
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Region { get; set; } = string.Empty;
-        public string Abrv { get; set; } = string.Empty;
-        public string Conference { get; set; } = string.Empty;
-        public bool IsHuman { get; set; } = true;
-        public List<Player> Players { get; set; } = new List<Player>();
-        public List<TeamGameStats> Stats { get; set; } = new List<TeamGameStats>();
-        public List<TeamDraftPicks> DraftPicks { get; set; } = new List<TeamDraftPicks>();
-        public TeamGameplan Gameplan { get; set; }
-        public List<TeamRegularStats> TeamRegularStats { get; set; } = new List<TeamRegularStats>();
-        public List<TeamPlayoffsStats> TeamPlayoffsStats { get; set; } = new List<TeamPlayoffsStats>();
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Region { get; set; } = string.Empty;
+		public string Abrv { get; set; } = string.Empty;
+		public string Conference { get; set; } = string.Empty;
+		public bool IsHuman { get; set; } = true;
+		public int Championships { get; set; }
+		public List<Player> Players { get; set; } = new List<Player>();
+		public List<TeamGameStats> Stats { get; set; } = new List<TeamGameStats>();
+		public List<TeamDraftPicks> DraftPicks { get; set; } = new List<TeamDraftPicks>();
+		public TeamGameplan Gameplan { get; set; }
+		public List<TeamRegularStats> TeamRegularStats { get; set; } = new List<TeamRegularStats>();
+		public List<TeamPlayoffsStats> TeamPlayoffsStats { get; set; } = new List<TeamPlayoffsStats>();
 
-        [NotMapped]
-        public TeamCompositeRating CompositeRating { get; set; }
-        [NotMapped]
-        public TeamSynergy Synergy { get; set; }
+		[NotMapped]
+		public TeamCompositeRating CompositeRating { get; set; }
+		[NotMapped]
+		public TeamSynergy Synergy { get; set; }
 
-    }
+	}
 
 }
