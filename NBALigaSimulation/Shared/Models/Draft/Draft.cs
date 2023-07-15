f40ea0ad75c998b14c4557e1a92434e4dda97d3b
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-15 18:21:33 -0300

    Criando classe Draft

---
+++ NBALigaSimulation/Shared/Models/Draft/Draft.cs
@@ -0,0 +1,18 @@
+﻿namespace NBALigaSimulation.Shared.Models
+{
+    public class Draft
+    {
+
+        public int Id { get; set; }
+        public int Season { get; set; }
+        public int Round { get; set; }
+        public int Pick { get; set; }
+        public Team Team { get; set; }
+        public string Original { get; set; }
+        public int? PlayerId { get; set; }
+        public Player? Player { get; set; }
+        public DateTime? DateTime { get; set; }
+
+    }
+
+}
