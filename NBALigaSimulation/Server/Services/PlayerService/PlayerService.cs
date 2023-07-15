c6c5a7772a0a32503393e0fe6c6ffc8368e56f6e
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-15 11:22:05 -0300

    Adicionando Playoffs Stats na Page do Player no Client

--- NBALigaSimulation/Server/Services/PlayerService/PlayerService.cs
+++ NBALigaSimulation/Server/Services/PlayerService/PlayerService.cs
@@ -2,174 +2,174 @@
 
 namespace NBALigaSimulation.Server.Services.PlayerService
 {
-	public class PlayerService : IPlayerService
-	{
-		private readonly DataContext _context;
-		private readonly IMapper _mapper;
+    public class PlayerService : IPlayerService
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
 
-		public PlayerService(DataContext context, IMapper mapper)
-		{
-			_context = context;
-			_mapper = mapper;
-		}
+        public PlayerService(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
-		public async Task<ServiceResponse<PlayerCompleteDto>> CreatePlayer(CreatePlayerDto request)
-		{
+        public async Task<ServiceResponse<PlayerCompleteDto>> CreatePlayer(CreatePlayerDto request)
+        {
 
-			ServiceResponse<PlayerCompleteDto> response = new ServiceResponse<PlayerCompleteDto>();
+            ServiceResponse<PlayerCompleteDto> response = new ServiceResponse<PlayerCompleteDto>();
 
-			Player player = _mapper.Map<Player>(request);
+            Player player = _mapper.Map<Player>(request);
 
-			_context.Add(player);
+            _context.Add(player);
 
-			await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
-			response.Success = true;
-			response.Data = _mapper.Map<PlayerCompleteDto>(player);
-			return response;
-		}
+            response.Success = true;
+            response.Data = _mapper.Map<PlayerCompleteDto>(player);
+            return response;
+        }
 
-		public async Task<ServiceResponse<List<PlayerCompleteDto>>> CreatePlayers(List<CreatePlayerDto> requests)
-		{
-			ServiceResponse<List<PlayerCompleteDto>> response = new ServiceResponse<List<PlayerCompleteDto>>();
+        public async Task<ServiceResponse<List<PlayerCompleteDto>>> CreatePlayers(List<CreatePlayerDto> requests)
+        {
+            ServiceResponse<List<PlayerCompleteDto>> response = new ServiceResponse<List<PlayerCompleteDto>>();
 
-			List<Player> players = _mapper.Map<List<Player>>(requests);
+            List<Player> players = _mapper.Map<List<Player>>(requests);
 
-			_context.AddRange(players);
+            _context.AddRange(players);
 
-			await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
-			response.Success = true;
-			response.Data = _mapper.Map<List<PlayerCompleteDto>>(players);
-			return response;
-		}
+            response.Success = true;
+            response.Data = _mapper.Map<List<PlayerCompleteDto>>(players);
+            return response;
+        }
 
 
-		public async Task<ServiceResponse<List<PlayerSimpleDto>>> GetAllPlayers()
-		{
-			var players = await _context.Players.ToListAsync();
-			var response = new ServiceResponse<List<PlayerSimpleDto>>
-			{
-				Data = _mapper.Map<List<PlayerSimpleDto>>(players)
-			};
+        public async Task<ServiceResponse<List<PlayerSimpleDto>>> GetAllPlayers()
+        {
+            var players = await _context.Players.ToListAsync();
+            var response = new ServiceResponse<List<PlayerSimpleDto>>
+            {
+                Data = _mapper.Map<List<PlayerSimpleDto>>(players)
+            };
 
-			return response;
-		}
+            return response;
+        }
 
-		public async Task<ServiceResponse<List<PlayerCompleteDto>>> GetAllFAPlayers()
-		{
-			var players = await _context.Players.Where(t => t.TeamId == 21)
-				.Include(p => p.Ratings)
-				.ToListAsync();
-			var response = new ServiceResponse<List<PlayerCompleteDto>>
-			{
-				Data = _mapper.Map<List<PlayerCompleteDto>>(players)
-			};
+        public async Task<ServiceResponse<List<PlayerCompleteDto>>> GetAllFAPlayers()
+        {
+            var players = await _context.Players.Where(t => t.TeamId == 21)
+                .Include(p => p.Ratings)
+                .ToListAsync();
+            var response = new ServiceResponse<List<PlayerCompleteDto>>
+            {
+                Data = _mapper.Map<List<PlayerCompleteDto>>(players)
+            };
 
-			return response;
-		}
+            return response;
+        }
 
-		public async Task<ServiceResponse<PlayerCompleteDto>> GetPlayerById(int playerId)
-		{
-			var response = new ServiceResponse<PlayerCompleteDto>();
-			var player = await _context.Players
-				.Include(t => t.Team)
-				.Include(p => p.Ratings)
-				.Include(p => p.Contract)
-				.Include(p => p.RegularStats)
-				.Include(p => p.Stats)
-				.Include(p => p.PlayoffsStats)
-				.Include(p => p.PlayerAwards)
-				.FirstOrDefaultAsync(p => p.Id == playerId);
+        public async Task<ServiceResponse<PlayerCompleteDto>> GetPlayerById(int playerId)
+        {
+            var response = new ServiceResponse<PlayerCompleteDto>();
+            var player = await _context.Players
+                .Include(t => t.Team)
+                .Include(p => p.Ratings)
+                .Include(p => p.Contract)
+                .Include(p => p.RegularStats)
+                .Include(p => p.Stats)
+                .Include(p => p.PlayoffsStats)
+                .Include(p => p.PlayerAwards)
+                .FirstOrDefaultAsync(p => p.Id == playerId);
 
-			if (player == null)
-			{
-				response.Success = false;
-				response.Message = $"O Player com o Id {playerId} não existe!";
-			}
-			else
-			{
+            if (player == null)
+            {
+                response.Success = false;
+                response.Message = $"O Player com o Id {playerId} não existe!";
+            }
+            else
+            {
 
-				response.Data = _mapper.Map<PlayerCompleteDto>(player);
-			}
+                response.Data = _mapper.Map<PlayerCompleteDto>(player);
+            }
 
-			return response;
-		}
+            return response;
+        }
 
-		public async Task<ServiceResponse<bool>> UpdateRosterOrder(List<PlayerCompleteDto> updatedPlayerList)
-		{
-			var response = new ServiceResponse<bool>();
+        public async Task<ServiceResponse<bool>> UpdateRosterOrder(List<PlayerCompleteDto> updatedPlayerList)
+        {
+            var response = new ServiceResponse<bool>();
 
-			foreach (var player in updatedPlayerList)
-			{
-				var playerDb = await _context.Players.FirstOrDefaultAsync(p => p.Id == player.Id);
+            foreach (var player in updatedPlayerList)
+            {
+                var playerDb = await _context.Players.FirstOrDefaultAsync(p => p.Id == player.Id);
 
-				if (playerDb == null)
-				{
-					response.Success = false;
-					response.Message = $"O Player com o Id {player.Id} não existe!";
-					return response;
+                if (playerDb == null)
+                {
+                    response.Success = false;
+                    response.Message = $"O Player com o Id {player.Id} não existe!";
+                    return response;
 
-				}
-				else
-				{
-					int rosterOrder = updatedPlayerList.IndexOf(player);
-					playerDb.RosterOrder = rosterOrder;
-				}
+                }
+                else
+                {
+                    int rosterOrder = updatedPlayerList.IndexOf(player);
+                    playerDb.RosterOrder = rosterOrder;
+                }
 
-			}
+            }
 
-			await _context.SaveChangesAsync();
-			response.Success = true;
-			response.Message = "Roster order updated successfully.";
+            await _context.SaveChangesAsync();
+            response.Success = true;
+            response.Message = "Roster order updated successfully.";
 
-			return response;
-		}
+            return response;
+        }
 
-		public async Task<ServiceResponse<bool>> UpdatePlayerPtModifier(int playerId, double newPtModifier)
-		{
-			var response = new ServiceResponse<bool>();
+        public async Task<ServiceResponse<bool>> UpdatePlayerPtModifier(int playerId, double newPtModifier)
+        {
+            var response = new ServiceResponse<bool>();
 
-			var player = await _context.Players.Include(t => t.Team).FirstOrDefaultAsync(p => p.Id == playerId);
+            var player = await _context.Players.Include(t => t.Team).FirstOrDefaultAsync(p => p.Id == playerId);
 
-			if (player == null)
-			{
-				response.Success = false;
-				response.Message = $"O Player com o Id {playerId} não existe!";
-				return response;
-			}
-			else
-			{
-				player.PtModifier = newPtModifier;
-			}
+            if (player == null)
+            {
+                response.Success = false;
+                response.Message = $"O Player com o Id {playerId} não existe!";
+                return response;
+            }
+            else
+            {
+                player.PtModifier = newPtModifier;
+            }
 
-			await _context.SaveChangesAsync();
-			response.Success = true;
-			response.Message = "PtModifier updated successfully.";
+            await _context.SaveChangesAsync();
+            response.Success = true;
+            response.Message = "PtModifier updated successfully.";
 
-			return response;
-		}
+            return response;
+        }
 
-		public async Task<ServiceResponse<bool>> GenerateContracts()
-		{
-			var response = new ServiceResponse<bool>();
+        public async Task<ServiceResponse<bool>> GenerateContracts()
+        {
+            var response = new ServiceResponse<bool>();
 
-			List<Player> players = await _context.Players.OrderBy(p => p.Id).Include(p => p.Ratings).ToListAsync();
+            List<Player> players = await _context.Players.OrderBy(p => p.Id).Include(p => p.Ratings).ToListAsync();
 
-			Season season = await _context.Seasons.OrderBy(s => s.Id).LastOrDefaultAsync();
+            Season season = await _context.Seasons.OrderBy(s => s.Id).LastOrDefaultAsync();
 
-			foreach (Player player in players)
-			{
-				var contract = ArrayHelper.GenContract(player, season, true, true, false);
-				player.Contract = contract;
-			}
+            foreach (Player player in players)
+            {
+                var contract = ArrayHelper.GenContract(player, season, true, true, false);
+                player.Contract = contract;
+            }
 
-			await _context.SaveChangesAsync();
-			response.Success = true;
-			response.Message = "Contracts generated successfully.";
+            await _context.SaveChangesAsync();
+            response.Success = true;
+            response.Message = "Contracts generated successfully.";
 
-			return response;
-		}
+            return response;
+        }
 
-	}
+    }
 }
