1dd8d88f87a26fad816a008d11ee3e84b3802741
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-07-19 13:18:31 -0300

    Criando DraftPlayerDto para passar Do Client para o Server

---
+++ NBALigaSimulation/Shared/Dtos/Draft/DraftPlayerDto.cs
@@ -0,0 +1,14 @@
+﻿namespace NBALigaSimulation.Shared.Dtos
+{
+    public class DraftPlayerDto
+    {
+
+        public int PlayerId { get; set; }
+        public int TeamId { get; set; }
+        public string Team { get; set; }
+        public int Year { get; set; }
+        public int Round { get; set; }
+        public int Pick { get; set; }
+
+    }
+}
