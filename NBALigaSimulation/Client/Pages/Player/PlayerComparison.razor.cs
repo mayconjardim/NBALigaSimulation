a545f9195ec54e423c2816107a9ac1db699e5a59
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-08-01 22:03:16 -0300

    Criando Page PlayerComparison no Client

---
+++ NBALigaSimulation/Client/Pages/Player/PlayerComparison.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NBALigaSimulation.Client.Pages.Player
+{
+    partial class PlayerComparison
+    {
+
+        private PlayerCompleteDto? playerOne = null;
+        private PlayerCompleteDto? playerTwo = null;
+        private string message = string.Empty;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            message = "Carregando Jogador...";
+
+            var result = await PlayerService.GetPlayerById(Id);
+            if (!result.Success)
+            {
+                message = result.Message;
+            }
+            else
+            {
+                playerOne = result.Data;
+            }
+        }
+
+    }
+}
