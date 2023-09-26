361375ce4883337f9db4140f40682950473ff1af
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-09-26 20:06:45 -0300

    Ajustando chart de Ratings

--- NBALigaSimulation/Client/Pages/Player/PlayerRatings.razor.cs
+++ NBALigaSimulation/Client/Pages/Player/PlayerRatings.razor.cs
@@ -123,14 +123,14 @@ namespace NBALigaSimulation.Client.Pages.Player
                 {
                     Labels = new List<string>()
                     {
-                      "DIQ", "REB", "OIQ", "HGT", "STR", "SPD", "JMP", "END"
+                      "OIQ", "DIQ", "REB", "HGT", "STR", "SPD", "JMP", "END"
                     },
                     Datasets = new List<ChartJsDataset>()
                     {
                         new RadarDataset()
                         {
                             Label = "",
-                            Data = new List<object>() { rating.Diq, rating.Reb, rating.Oiq, rating.Hgt, rating.Stre, rating.Spd, rating.Jmp, rating.Endu },
+                            Data = new List<object>() {  rating.Oiq, rating.Diq, rating.Reb, rating.Hgt, rating.Stre, rating.Spd, rating.Jmp, rating.Endu },
                             BackgroundColor = "rgba(3, 35, 76, 0.83)",
                             BorderColor = "#03234C",
                             BorderWidth = 3
