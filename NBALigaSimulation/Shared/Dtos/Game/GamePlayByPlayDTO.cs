e64502ab3b5b0a6ab4e806521e232ab55a7f6342
Maycon Parreira Jardim <mayconsmill@gmail.com>
2023-10-07 22:35:54 -0300

    Criando GamePlayByPlayDTO

---
+++ NBALigaSimulation/Shared/Dtos/Game/GamePlayByPlayDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace NBALigaSimulation.Shared.Dtos.Game
+{
+    public class GamePlayByPlayDTO
+    {
+        public int? Id { get; set; }
+        public int? GameSimId { get; set; }
+        public string? Type { get; set; }
+        public string? Text { get; set; }
+        public string? TeamName { get; set; }
+        public double? T { get; set; }
+        public string? Time { get; set; }
+        public int? On { get; set; }
+        public int? Off { get; set; }
+    }
+}
