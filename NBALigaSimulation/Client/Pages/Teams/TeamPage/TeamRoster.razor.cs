2a6448c2067f87ebbff3754ae43fcfaca9145dc9
Maycon Jardim <mayconsmill@gmail.com>
2024-03-15 20:56:05 -0300

    Adicionando img no TeamRoster

--- NBALigaSimulation/Client/Pages/Teams/TeamPage/TeamRoster.razor.cs
+++ NBALigaSimulation/Client/Pages/Teams/TeamPage/TeamRoster.razor.cs
@@ -1,8 +1,30 @@
+using Microsoft.AspNetCore.Components;
+using NBALigaSimulation.Shared.Dtos.Players;
+using NBALigaSimulation.Shared.Dtos.Teams;
+
 namespace NBALigaSimulation.Client.Pages.Teams.TeamPage;
 
 public partial class TeamRoster
 {
     
+    [Parameter] 
+    public TeamCompleteDto _team { get; set; }
+
+    List<PlayerCompleteDto> _roster { get; set; }
     
+    string[] _headings =
+    {
+        "PLAYER", "POS"
+    };
+    
+    protected override async Task OnInitializedAsync()
+    {
+
+        if (_team.Players != null)
+        {
+            _roster = _team.Players;
+        }
+       
+    }
     
 }
