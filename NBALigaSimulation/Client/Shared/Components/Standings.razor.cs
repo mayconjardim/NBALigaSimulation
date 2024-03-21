e4168b0d5bdee40bb919e8a5b954848633da4777
Maycon Jardim <mayconsmill@gmail.com>
2024-03-20 22:07:55 -0300

    Adicionando Standings para teste e Leauge Leaders

---
+++ NBALigaSimulation/Client/Shared/Components/Standings.razor.cs
@@ -0,0 +1,84 @@
+using System.Globalization;
+using NBALigaSimulation.Shared.Dtos.Teams;
+
+namespace NBALigaSimulation.Client.Shared.Components;
+
+public partial class Standings
+{
+    
+          private List<TeamRegularStatsDto> statsEast = new List<TeamRegularStatsDto>();
+        private List<TeamRegularStatsDto> statsWest = new List<TeamRegularStatsDto>();
+
+        private string message = string.Empty;
+
+        string[] east = { "EAST", "W-L", "PCT", "PF", "PA", "DIFF", "STRK" };
+        string[] west = { "WEST", "W-L", "PCT", "PF", "PA", "DIFF", "STRK" };
+        
+        protected override async Task OnInitializedAsync()
+        {
+            message = "Carregando Stats...";
+
+            var result = await StatsService.GetAllTeamRegularStats();
+            if (!result.Success)
+            {
+                message = result.Message;
+            }
+            else
+            {
+                statsEast = result.Data.OrderByDescending(t => t.WinPct).Where(t => t.TeamConference == "East").ToList();
+                statsWest = result.Data.OrderByDescending(t => t.WinPct).Where(t => t.TeamConference == "West").ToList();
+            }
+
+        }
+
+        private string GetFormattedDiff(string diff)
+        {
+            double diffValue = double.Parse(diff, CultureInfo.InvariantCulture);
+            string formattedDiff = diffValue.ToString("0.0", CultureInfo.InvariantCulture);
+
+            if (diffValue > 0)
+            {
+                formattedDiff = "+" + formattedDiff;
+            }
+
+            return formattedDiff;
+        }
+
+        private string GetDiffStyle(string diff)
+        {
+            double diffValue = double.Parse(diff, CultureInfo.InvariantCulture);
+
+            string style = "";
+
+            if (diffValue < 0)
+            {
+                style = "color: red";
+            }
+            else if (diffValue > 0)
+            {
+                style = "color: green";
+                diff = "+" + diff; 
+            }
+
+            return $"{style}; font-weight: bold";
+        }
+
+        private string GetStreak(int streak)
+        {
+
+            if (streak > 0)
+            {
+                return "W" + streak;
+            }
+            else if (streak < 0)
+            {
+                return "L" + Math.Abs(streak);
+            }
+            else
+            {
+                return "L1";
+            }
+
+        }
+    
+}
