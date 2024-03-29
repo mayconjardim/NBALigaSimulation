1b072dc72a37db0d7c8447e7fe0e31b06f75d685
Maycon Jardim <mayconsmill@gmail.com>
2024-03-29 20:24:38 -0300

    Instalando e Configurando radzen

--- NBALigaSimulation/Client/Pages/Players/PlayerComparison/PlayerComparison.razor.cs
+++ NBALigaSimulation/Client/Pages/Players/PlayerComparison/PlayerComparison.razor.cs
@@ -15,7 +15,6 @@ public partial class PlayerComparison
     private PlayerRegularStatsDto _player2Stats;
     private PlayerRatingDto _playerRating1;
     private PlayerRatingDto _playerRating2;
-    private List<PlayerSimpleDto> suggestions = new List<PlayerSimpleDto>();
     private int _season = 0;
     private string searchText = string.Empty;
     private string searchText2 = string.Empty;
@@ -23,29 +22,6 @@ public partial class PlayerComparison
     protected override async Task OnInitializedAsync()
     {
         _season = _season = int.Parse(await LocalStorage.GetItemAsync<string>("season"));
-    }
-    
-    private async Task<AutoCompleteDataProviderResult<PlayerSimpleDto>> CustomersDataProvider(AutoCompleteDataProviderRequest<PlayerSimpleDto> request)
-    {
-
-        var response  = await PlayerService.GetPlayersSearchSuggestions(searchText);
-        List<PlayerSimpleDto> players = new List<PlayerSimpleDto>();
-        if (response.Success)
-        {
-            players = response.Data;
-        }
-        return await Task.FromResult(new AutoCompleteDataProviderResult<PlayerSimpleDto> { Data = players, TotalCount = players.Count() });
-    }
-    
-    private async void OnAutoCompleteChanged(PlayerSimpleDto player)
-    {
-        var response  = await PlayerService.GetPlayerById(player.Id);
-        if (response.Success)
-        {
-                _player1 = response.Data;
-                _player1Stats = _player1.RegularStats.LastOrDefault();
-                _playerRating1 = _player1.Ratings.LastOrDefault();
-        }
     }
     
     
