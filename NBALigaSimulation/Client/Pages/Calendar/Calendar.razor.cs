38601e6ab71926f7cd592bf7e4c4ce97aef8fc68
Maycon Jardim <mayconsmill@gmail.com>
2024-04-07 09:12:46 -0300

    Ajustando Calendar e TeamSchedule

--- NBALigaSimulation/Client/Pages/Calendar/Calendar.razor.cs
+++ NBALigaSimulation/Client/Pages/Calendar/Calendar.razor.cs
@@ -8,7 +8,7 @@ public partial class Calendar
     private List<GameCompleteDto> _games = null;
     Dictionary<DateTime, List<GameCompleteDto>> gamesByDate = new Dictionary<DateTime, List<GameCompleteDto>>();
         
-    string[] headings = { "DAY", "HOME", "AWAY", "W-L", "RESULT" };
+    string[] headings = { "DAY", "HOME", "AWAY", "RESULT" };
     
     protected override async Task OnInitializedAsync()
     {
--- NBALigaSimulation/Shared/Models/Seasons/Season.cs
+++ NBALigaSimulation/Shared/Models/Seasons/Season.cs
@@ -9,17 +9,21 @@ namespace NBALigaSimulation.Shared.Models.Seasons
 
         public int Id { get; set; }
         public int Year { get; set; }
-        public List<Game> Games { get; set; }
+    
         public bool IsCompleted { get; set; } = false;
         public bool LotteryCompleted { get; set; } = false;
         public bool DraftCompleted { get; set; } = false;
         public bool TcCompleted { get; set; } = false;
         public bool DeadlineCompleted { get; set; } = false;
         public bool RegularCompleted { get; set; } = false;
+        
+        //Playoffs
         public bool FirstRoundCompleted { get; set; } = false;
         public bool SecondRoundCompleted { get; set; } = false;
         public bool ThirdRoundCompleted { get; set; } = false;
         public bool FourthRoundCompleted { get; set; } = false;
+        
+        public List<Game> Games { get; set; }
 
     }
 }
