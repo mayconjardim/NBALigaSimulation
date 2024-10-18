7fbc1cb432525a763d2c8105f3a8195b4b230714
Maycon Jardim <mayconsmill@gmail.com>
2024-10-17 22:33:16 -0300

    Criando tela de edição de jogador no Client

---
+++ NBALigaSimulation/Client/Pages/Players/PlayerPage/PlayerEdit.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using NBALigaSimulation.Shared.Dtos.Players;
+
+namespace NBALigaSimulation.Client.Pages.Players.PlayerPage;
+
+public partial class PlayerEdit
+{
+    
+    [Parameter]
+    public PlayerCompleteDto _player { get; set; }
+    private PlayerRatingDto _rating;
+
+    private async Task HandleValidSubmit()
+    {
+    }
+
+}
